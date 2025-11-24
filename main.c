2aab177f6db982c5d2abb7efc101e5a2ac559400
Sruthika <sruthika1907@gmail.com>
2025-11-24 20:54:07 +0530

    main.c Added

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "availability.h"
+#include "booking.h"
+#include "global.h"
+#include "passenger.h"
+#include "system-setup.h"
+#include <stdio.h>
+
+int main()
+{
+    initializeSystem();
+    int choice;
+    do 
+    {
+        displayMenu();
+        printf("Enter Choice (1 - 5)\n");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            booking();
+            break;
+
+        case 2:
+            checkAvailability();
+            break;
+
+        case 3:
+            cancellation();
+            break;
+
+        case 4:
+            prepareChart();
+            break;
+        
+        case 5:
+            printf("Exiting System...\n");
+            break;
+        
+        default:
+            printf("Invalid Entry...!\n");
+            break;
+        }
+    } while (choice != 5);
+
+    return 0;
+}
