0d122d4d6160595fd2a1d66b4311eba87db198fe
Sruthika <sruthika1907@gmail.com>
2025-11-24 20:38:55 +0530

    passenger.h Added

--- passenger.h
+++ passenger.h
@@ -1,11 +1,13 @@
 #ifndef PASSENGER_H
 #define PASSENGER_H
 
+typedef enum { MALE, FEMALE } Gender;
+
 typedef struct 
 {
     char name[20];
     int age;
-    char gender;
+    Gender gender;
     int couchType;
     int seatNo;
     int pnr;
