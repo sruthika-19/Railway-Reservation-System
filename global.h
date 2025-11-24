0853692c71b80c6495a5a2383aa104a9f2f9f6c3
Sruthika <sruthika1907@gmail.com>
2025-11-24 20:32:05 +0530

    global.h Added

---
+++ global.h
@@ -0,0 +1,23 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include <stdio.h>
+#include <string.h>
+#include "passenger.h"
+
+#define MAX_AC_SEAT 10;
+#define MAX_NONAC_SEAT 10;
+#define MAX_SEATER_SEAT 10;
+#define MAX_WAITING_LIST 3;
+
+extern Passenger acCouch[MAX_AC_SEAT];
+extern Passenger nonacCouch[MAX_NONAC_SEAT];
+extern Passenger seaterCouch[MAX_SEATER_SEAT];
+extern Passenger waitingList[MAX_WAITING_LIST];
+
+extern int acCount;
+extern int nonAcCount;
+extern int seaterCount;
+extern int wlCount;
+
+#endif
