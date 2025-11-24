288537a34c6d8eb3ebbadc648ce31630961887c7
Sruthika <sruthika1907@gmail.com>
2025-11-24 20:20:20 +0530

    Passenger.h Added

---
+++ passenger.h
@@ -0,0 +1,15 @@
+#ifndef PASSENGER_H
+#define PASSENGER_H
+
+typedef struct 
+{
+    char name[20];
+    int age;
+    char gender;
+    int couchType;
+    int seatNo;
+    int pnr;
+
+} Passenger;
+
+#endif
