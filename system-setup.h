a2e23434e0f6511c40ce156f129b6bbd434c1868
rsharitha25csec07 <rsharitha25csec07@gmail.com>
2025-12-06 21:19:16 +0530

    system setup added

---
+++ system-setup.h
@@ -0,0 +1,13 @@
+#ifndef SETUP_SYSTEM_H
+#define SETUP_SYSTEM_H
+
+
+ 
+void displayMenu();
+void prepareChart();
+void initializeSystem();
+void saveChartToFile();
+void loadSystemData();
+void saveSystemData();
+
+#endif
